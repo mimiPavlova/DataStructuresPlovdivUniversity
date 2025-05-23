3f3ec24a4e9e08bbe8ae10ba7c5c7f9aeeedc7a5
Мария Павлова <ivana.prego@gmail.com>
2025-05-23 14:06:47 +0300

    😺

---
+++ 5.Binary-search-and-merge-sort/BinarySearch/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace BinarySearch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Двоичното търсене (binary search) е алгоритъм за търсене в
+             * сортиран масив, при който се използва разделяй 
+             * и владей подход. На всяка стъпка се избира средният 
+             * елемент и се решава дали да се търси наляво или надясно.*/
+            Console.WriteLine(BinarySearch(new int[] {1,2,3,4,5}, 5));
+        }
+        static int BinarySearch(int[] arr, int key)
+        {
+            int left = 0;
+            int right= arr.Length - 1;
+            while (left<=right)
+            {
+                int mid = left+(right-left)/2;
+                if (arr[mid]==key) return mid;
+                else if (arr[mid]<key) left=mid+1;
+                else right=mid-1;
+            }
+            return -1;
+        }
+    }
+}
---
+++ 5.Binary-search-and-merge-sort/MergeSort/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace MergeSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 2, 5, 4, 1, 6, 8, 9, 0, 7 };
+            MergeSort(arr);
+            Console.WriteLine(string.Join(" ", arr));
+        }
+        static void MergeSort(int[] arr)
+        {
+            if (arr.Length<=1) return;
+            int mid = arr.Length/2;
+            int[] left = arr.Take(mid).ToArray();
+            int[] right=arr.Skip(mid).ToArray();
+
+            MergeSort(left);
+            MergeSort(right);
+
+            Merge(arr, left, right);
+
+        }
+        static void Merge(int[]result, int[]left, int[]right)
+        {
+            int i=0, j=0, k = 0;
+
+            // Сливане на ляв и десен масив
+            while (i<left.Length&&j<right.Length)
+            {
+                if (left[i]<=right[j])
+                {
+                    result[k++] = left[i++];
+                }
+                else
+                {
+                    result[k++]=right[j++];
+                }
+            }
+
+            //Добавяне на останалите елементи
+            while (i<left.Length)
+            {
+                result[k++]=left[i++];
+
+            }
+            while (j<right.Length)
+            {
+                result[k++]=right[j++];
+            }
+
+        }
+    }
+}
