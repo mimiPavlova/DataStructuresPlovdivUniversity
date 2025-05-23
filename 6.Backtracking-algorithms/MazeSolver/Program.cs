9d9b71b131b7cf794ba34a5150fba2f293434f4b
Мария Павлова <ivana.prego@gmail.com>
2025-05-23 15:10:35 +0300

    😺

---
+++ 6.Backtracking-algorithms/MazeSolver/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace MazeSolver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] maze =
+            {
+                {1, 0, 1, 1 },
+                {1, 1, 1, 0 },
+                {0, 1, 0, 1 },
+                {1, 1, 1, 1, }
+            };
+            int n=maze.GetLength(0);
+            bool[,] visited = new bool[n,n];
+            if(!SolveMaze(0, 0, maze, visited))
+            {
+                Console.WriteLine("Нама път от началото до края.");
+            }
+
+        }
+        static bool SolveMaze(int x, int y, int[,]maze, bool[,] visited)
+        {
+            int n=maze.GetLength(0);
+            if (x<0||y<0||y>=n||maze[x, y]==0||visited[x, y])
+                return false;
+
+            visited[x, y]=true;
+            if (x==n-1&&y==n-1)
+            {
+                Console.WriteLine($"Крайна точка достигната при ({x} {y})");
+                return true;
+            }
+
+            //Движения: надясно, надолу, налаво, нагоре
+
+            int[] dx = { 0, 1, 0, -1 };  
+            int[] dy = { 1, 0, -1, 0 };  
+
+            for (int i = 0; i < 4; i++)
+            {
+                int newX = x+dx[i];
+                int newY=y+dy[i];
+                if(SolveMaze(newX, newY, maze, visited))
+                {
+                    Console.WriteLine($"Част от пътя ({x}, {y}) ");
+                    return true;
+                }
+
+            }
+            visited[x, y]=false;
+            return false;
+
+
+        }
+    }
+}
---
+++ 6.Backtracking-algorithms/NQueens/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace NQueens
+{
+    internal class Program
+    {
+        const int N = 8;
+        static void Main(string[] args)
+        {
+            int[] board = new int[N];
+            PlaceQueen(0, board);
+        }
+        static void PlaceQueen(int row, int[] board)
+        {
+            if(row==N)
+            {
+                PrintBoard(board);
+                return;
+            }
+
+            for (int col = 0; col < N; col++)
+            {
+                if(CanPlace(row, col, board))
+                {
+                    board[row]=col;
+                    PlaceQueen(row+1, board);
+                }
+
+            }
+        }
+        static bool CanPlace(int row,int col, int[] board)
+        {
+            for (int i = 0; i < row; i++)
+            {
+                if (board[i]==col||Math.Abs(board[i]-col)
+                    ==Math.Abs(i-row)) return false;
+
+            }
+            return true;
+        }
+
+
+        static void PrintBoard(int[] board)
+        {
+            for (int row = 0; row < N; row++)
+            {
+                for (int col = 0; col < N; col++)
+                {
+                    Console.Write(board[row]==col?"f":".");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
