bd2f91f09022955e385a1d2bb7cc2f006980f73c
Мария Павлова <ivana.prego@gmail.com>
2025-05-25 19:56:56 +0300

    😺

---
+++ 07.LinkedList/Implementation/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Implementation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           CustomLinkedList list= new CustomLinkedList();
+            list.AddFirst(1);
+            Console.WriteLine(list.First.Value);
+
+        }
+
+    }
+}
---
+++ 07.LinkedList/ListImplementation/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ListImplementation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
