ce617029dd3eef517de2c5d7b777ca8dafb3fd40
Мария Павлова <ivana.prego@gmail.com>
2025-05-28 23:05:33 +0300

    🐸

---
+++ 12.AdjacencyMatrix/demo/Graph.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo
+{
+    public class Graph
+    {
+        private int[,] adjacencyMatrix;
+        private int numVertices;
+
+        public Graph(int vertices)
+        {
+            numVertices=vertices;
+            adjacencyMatrix=new int[vertices, vertices];
+
+        }
+        
+        public void AddEdge(int i, int j)
+        {
+            if (i>=0&&i<numVertices&&j>=0&&j<numVertices)
+            {
+                adjacencyMatrix[i, j]=1;
+                adjacencyMatrix[j, i]=1;
+            }
+
+        }
+        public void RemoveEdge(int i, int j)
+        {
+            if (i>=0&&i<numVertices&&j>=0&&j<numVertices)
+            {
+                adjacencyMatrix[i, j]=0;
+                adjacencyMatrix[j, i]=0;
+
+            }
+
+        }
+        public bool HasEdge(int i, int j)
+        {
+            return adjacencyMatrix[i, j]==1;
+
+        }
+        public void PrintMatrix()
+        {
+            Console.WriteLine("Матрица на съседствата:");
+
+            for (int i = 0; i < numVertices; i++)
+            {
+                for (int j = 0; j < numVertices; j++)
+                {
+                  Console.Write(adjacencyMatrix[i,j]+" ");
+                }
+            }
+            Console.WriteLine();
+        }
+
+    
+}
---
+++ 12.AdjacencyMatrix/demo/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph graph = new Graph(4);
+            graph.AddEdge(0, 1);
+            graph.AddEdge(0, 2);
+            graph.AddEdge(1, 2);
+            graph.AddEdge(2, 3);
+            graph.PrintMatrix();
+
+            Console.WriteLine("Има ли ръб между 1 и 3"+graph.HasEdge(1, 3));
+            Console.WriteLine("Има ли ръб между 2 и 3"+graph.HasEdge(2, 3));
+        }
+    }
+}
