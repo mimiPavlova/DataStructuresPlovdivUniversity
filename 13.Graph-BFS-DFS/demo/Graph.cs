248f6d4478e3cc50cd75afc4bb641eca5899c18e
Мария Павлова <ivana.prego@gmail.com>
2025-05-30 17:27:03 +0300

    😺

--- 13.Graph-BFS-DFS/demo/Graph.cs
+++ 13.Graph-BFS-DFS/demo/Graph.cs
@@ -17,7 +17,7 @@ public class Graph
         this.adjacencyMatrix=new int[vertices, vertices];
         visited = new bool[vertices];
     }
-    private void AddEdge(int i, int j)
+    public void AddEdge(int i, int j)
     {
         if (i>=0 && j>=0&&i<numVertices&&j<numVertices)
         {
@@ -27,6 +27,48 @@ public class Graph
         else throw new ArgumentOutOfRangeException("index out of range");
 
     }
-    //
+    //Обхождане в широчина
+    public void BFS(int start)
+    {
+        visited =new bool[numVertices];
+        Queue<int>queue = new Queue<int>();
+
+        visited[start] = true;
+        queue.Enqueue(start);
+
+        while (queue.Count>0)
+        {
+            int v = queue.Dequeue();
+            Console.WriteLine(v+" ");
+            for (int i = 0; i < numVertices; i++)
+            {
+                if (adjacencyMatrix[v, i]==1&&!visited[i])
+                {
+                    visited[i]=true;
+                    queue.Enqueue(i);
+                }
+            }
+        }
+    }
+    //Обхождане в дълбочина
+    public void DFS(int strat)
+    {
+        visited=new bool[numVertices];
+        DFSUntil(strat);
+
+    }
+   private void DFSUntil(int v)
+    {
+        visited[(int)v] = true;
+        Console.WriteLine(v+" ");
+        for (int i = 0; i < numVertices; i++)
+        {
+            if (adjacencyMatrix[v, i]==1&&!visited[i])
+            {
+                DFSUntil(i);
+            }
+        }
+    }
+
 }
 
--- 13.Graph-BFS-DFS/demo/Program.cs
+++ 13.Graph-BFS-DFS/demo/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-           
+            Graph g = new Graph(5);
+            g.AddEdge(0, 1);
+            g.AddEdge(0, 2);
+            g.AddEdge(1, 3);
+            g.AddEdge(2, 4);
+
+            Console.WriteLine("DFS from edge 0:");
+            g.DFS(0);
+            Console.WriteLine("\nBFS from edge 0:");
+            g.BFS(0);
+
         }
     }
 }
